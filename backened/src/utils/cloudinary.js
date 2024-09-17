3192ec212bc7990d8b6a525437e044dba0f814e0
Sumit Tiwari <sumit2004tiwari@gmail.com>
2024-09-17 10:46:47 +0530

    multer

---
+++ backened/src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from "cloudinary"
+import { response } from "express";
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (LocalFilePath){
+    try {
+        if(!LocalFilePath) return null
+
+        cloudinary.uploader.upload(LocalFilePath , {
+            resource_type : "auto"
+        })
+
+        console.log("file is uploaded" , response.url)
+         return response;
+
+
+    } catch (error) {
+       fs.unlinkSync(LocalFilePath);
+       return null
+    }
+}
+
+export {uploadOnCloudinary} 
